3fe3b674bf51cc4e3510e203599d06fc3f060260
Sylvieu25 <s.uwera2@alustudent.com>
2025-11-18 02:25:33 -0800

    Fix import module task with correct formatting and structure

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 from calculator_1 import add, sub, mul, div
 
-def main():
+if __name__ == "__main__":
     a = 10
     b = 5
 
@@ -10,6 +10,3 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
-if __name__ == "__main__":
-    main()
