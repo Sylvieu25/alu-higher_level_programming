8ff65a26c0e625990206a42feff94a38e530af57
Sylvieu25 <s.uwera2@alustudent.com>
2025-12-05 03:12:46 -0800

    Save local changes before rebase

--- 0-print_list_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Print all integers of a list, one per line."""
-    for i in my_list:
-        if isinstance(i, int):
-            print("{:d}".format(i))
-        else:
-            raise TypeError("List must contain only integers")
