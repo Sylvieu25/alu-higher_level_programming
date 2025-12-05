a95e771be9e4412eb461c7186ffadf38d24de9e9
Sylvieu25 <s.uwera2@alustudent.com>
2025-12-05 00:00:59 -0800

    Add 0-print_list_integer function and README

--- 0-print_list_integer.py
+++ 0-print_list_integer.py
@@ -2,4 +2,7 @@
 def print_list_integer(my_list=[]):
     """Print all integers of a list, one per line."""
     for i in my_list:
-        print("{}".format(i))
+        if isinstance(i, int):
+            print("{:d}".format(i))
+        else:
+            raise TypeError("List must contain only integers")
